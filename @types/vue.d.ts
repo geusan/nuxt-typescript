410948347cf31e296d2f034c1b6189bfebe10e19
geusan lim <dnay2k@taggers.io>
2019-01-25 12:16:04 +0900

    components 폴더에 있는 모든 파일들은 vue 형식으로 가져오도록 import 설정

--- @types/vue.d.ts
+++ @types/vue.d.ts
@@ -1,4 +1,8 @@
-declare module '*.vue' {
+declare module '~/components/*' {
+  import Vue from 'vue'
+  export default Vue
+}
+declare module '@/components/*' {
   import Vue from 'vue'
   export default Vue
 }
