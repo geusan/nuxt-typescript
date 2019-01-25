ca342ff161d4c06ad749b4385a32a938e366697d
geusan lim <dnay2k@taggers.io>
2019-01-25 12:33:07 +0900

    nuxt.config.ts 설정 파일 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
+const path = require('path')
+
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'Nuxt with typescript',
     meta: [
@@ -9,40 +11,45 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
-  ** Customize the progress bar color
-  */
+   ** Customize the progress bar color
+   */
   loading: { color: '#3B8070' },
   modules: [
     // if want to use nuxt-ts library
-    // "nuxt-ts"
+    // [
+    //   'nuxt-ts',
+    //   {
+    //     forkTsChecker: { workers: 2, memoryLimit: 4096 },
+    //     tsconfig: path.join(__dirname, 'tsconfig.json'),
+    //   },
+    // ],
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isServer }) {
+     ** Run ESLint on save
+     */
+    extend(config, { isServer }) {
       const tsLoader = {
         loader: 'ts-loader',
         options: {
           appendTsSuffixTo: [/\.vue$/],
           transpileOnly: true,
+          configFile: path.join(__dirname, 'tsconfig.json'),
         },
-        exclude: [
-          /vendor/,
-          /\.nuxt/,
-        ],
+        exclude: [/vendor/, /\.nuxt/],
       }
-      config.module.rules.push({ test: /((client|server)\.js)|(\.tsx?)$/, ...tsLoader })
+      config.module.rules.push({
+        test: /((client|server)\.js)|(\.tsx?)$/,
+        ...tsLoader,
+      })
 
-      config.module.rules = config.module.rules.map((rule) => {
+      config.module.rules = config.module.rules.map(rule => {
         if (rule.loader === 'vue-loader') {
           if (!rule.options.loaders) {
             rule.options.loaders = {}
@@ -57,7 +64,9 @@ module.exports = {
       if (config.resolve.extensions.indexOf('.tsx') < 0) {
         config.resolve.extensions.push('.tsx')
       }
-      if (isServer) { config.externals = [] }
+      if (isServer) {
+        config.externals = []
+      }
     },
   },
 }
